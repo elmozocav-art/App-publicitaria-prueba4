731a3a957f58df8f151b9a7c5239c99d0a80317f
elmozocav-art <elmozocav@gmail.com>
2026-02-25 15:46:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ def inicializar_google_sheets():
     try:
         # PANTILLA DIRECTA: Pegamos tus datos aquí para evitar errores de Secrets
         info = {
-          "type": "service_account",
-          "project_id": "automatizacion-darpepro",
-          "private_key_id": "7dec218613311a522ef8a09e4602ca15e804a4f7",
-          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDm4VNtSULgqEJJWTFleXuqbpcPlQlEFKCcuhmMZK7kOpBeLVG0j9ZxxM1VA+DDnBJ2TZjCvdnmd2/RQIwWETlVvqthrlhhXPrw6O6xz+2kmpC/Po3qcXTV2Vyz4B45tRxrnBg0pWqJOfsuLUytSn5UCJdTtNcUxsv4yMwP6TRsJMvdIJT5l51uq7zFat7faEupeZow8JSqD2b16N3MgoXYOecdnCDyPtBOjSqOOTTlcWhF5yd1dCShbg6KPeEmQ412cGTTnb8Q0iFlS2oLtLhx8ohJUXrGhpQVNxALSfauOACZC5PbBvp7y3AdBEqk7pWhc/a42BRhX48A+i5dDlMVAgMBAAECggEACYylqA4xAfHJAWz6/2N4RfhaWdP5lM31nAXmgJ65d8nO/CuLsZgxb8s1YvV3b8pzc7PfRE7DRdEILZ4p18/RO0auTVVUunzfXuTY2pSVvl/WP31q3pjgEuhGo/n6cLtDov/LJdehrG4FgZzGCx+Fp3F424bN+UIFCxPjEiq8dwHf0AVBm/vryLziUe1RhWhmU5PCqJQT7T5qCmYQiVtr2D6lDU2nWch7R3oKLhYA/yaWdp6whgmEQ5YPSmGiGEA+qLrvOX8pIIZk3k5MJDNuKzTHSEG71iVcW5wfaDceS/QbcemkUxvrFN1WGYe25X29thzi2pbRdFR758UVQQtSEQKBgQD+sdCW1oe+fGwjbmYbrJfvPPU2PB+Fj09lcadPHpsCzoW7N11yv/Lcmxl65Fkw31EFFtK7Aqrc9XE7l99Xs2pF1OezrTRkStGpn2/iuu3uFoLjE3s2lwg9rPfkJ5m06OY2ZPUTizv/M9fVM2SGiHwsCd2JP4faC9Eb2CV/XDjHcwKBgQDoEEOf1HfHVirpKTePsM5/Tnn6x3pgc45VjlHm4SigSI5Ft+bZ0Zqt0Ww56hP7emRb5GzGqoDIgxhhkzXx6X+Zo+bi2T9tMBLYyvkkGsva+1S2/lXRGppu/w5oDF/u4Cpu2FBXEG5ZBOvlUFTNUMU3/hhJwvIo6VUBzjMHU2KJVwKBgDHqT2MAcGIYqGjZPhG2ZaBxZe1XIyZdy8Wp3IxZBzApoXPridjvXX12uCupjQFTAuHocyxLfYkIYUS4owDhHLbK3w4Lp/Tv5N9Fa/wXfoHmz4gJTWCJgQf1Wi/QwwL9kgCfEJjf9MYzNt2F0PG81fkbhdtcJBfLsROv7MllHYuZAoGAfsmUCNjucCw12Zm5T3pL9+YKYudlhxjbZQcS3E+cAKZjhNfK+qq4Fctbk/C95iGif0o32/r/zY1CXt0tFfR60Jhp5vrG2oLvaR7MK9uwEP5L9IbUoCAmzAnx9wr7xukWheUNT+QcReqYb3sKOfqtJcBfyL0HzS5eUVQ1MQVvypUCgYEAmMNB+xZp8Vrwj4ZkHuyQV5NdVLRaGIRfmIyDm8YG9t4A1CR6Ezs4ZB+2kPSkoZFuZcU9iRKXn2F9jIknuW49zqt2VLtGBPj3UGwB+Tc4Q+k/Ytcz6bZXn8BIvBG15fNXpMFN6lekdvgZHV2wWvHJbIJFZOVb0MhDqKNtJ2e2J+E=\n-----END PRIVATE KEY-----\n",
-          "client_email": "bot-darpepro@automatizacion-darpepro.iam.gserviceaccount.com",
-          "client_id": "104849416098488477184",
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bot-darpepro%40automatizacion-darpepro.iam.gserviceaccount.com",
-          "universe_domain": "googleapis.com"
-        }
+  "type": "service_account",
+  "project_id": "automatizacion-darpepro",
+  "private_key_id": "7dec218613311a522ef8a09e4602ca15e804a4f7",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDm4VNtSULgqEJJ\nWTFleXuqbpcPlQlEFKCcuhmMZK7kOpBeLVG0j9ZxxM1VA+DDnBJ2TZjCvdnmd2/R\nQIwWETlVvqthrlhhXPrw6O6xz+2kmpC/Po3qcXTV2Vyz4B45tRxrnBg0pWqJOfsu\nLUytSn5UCJdTtNcUxsv4yMwP6TRsJMvdIJT5l51uq7zFat7faEupeZow8JSqD2b1\n6N3MgoXYOecdnCDyPtBOjSqOOTTlcWhF5yd1dCShbg6KPeEmQ412cGTTnb8Q0iFl\nS2oLtLhx8ohJUXrGhpQVNxALSfauOACZC5PbBvp7y3AdBEqk7pWhc/a42BRhX48A\n+i5dDlMVAgMBAAECggEACYylqA4xAfHJAWz6/2N4RfhaWdP5lM31nAXmgJ65d8nO\n/CuLsZgxb8s1YvV3b8pzc7PfRE7DRdEILZ4p18/RO0auTVVUunzfXuTY2pSVvl/W\nP31q3pjgEuhGo/n6cLtDov/LJdehrG4FgZzGCx+Fp3F424bN+UIFCxPjEiq8dwHf\n0AVBm/vryLziUe1RhWhmU5PCqJQT7T5qCmYQiVtr2D6lDU2nWch7R3oKLhYA/yaW\ndp6whgmEQ5YPSmGiGEA+qLrvOX8pIIZk3k5MJDNuKzTHSEG71iVcW5wfaDceS/Qb\ncemkUxvrFN1WGYe25X29thzi2pbRdFR758UVQQtSEQKBgQD+sdCW1oe+fGwjbmYb\nrJfvPPU2PB+Fj09lcadPHpsCzoW7N11yv/Lcmxl65Fkw31EFFtK7Aqrc9XE7l99X\ns2pF1OezrTRkStGpn2/iuu3uFoLjE3s2lwg9rPfkJ5m06OY2ZPUTizv/M9fVM2SG\niHwsCd2JP4faC9Eb2CV/XDjHcwKBgQDoEEOf1HfHVirpKTePsM5/Tnn6x3pgc45V\njlHm4SigSI5Ft+bZ0Zqt0Ww56hP7emRb5GzGqoDIgxhhkzXx6X+Zo+bi2T9tMBLY\nyvkkGsva+1S2/lXRGppu/w5oDF/u4Cpu2FBXEG5ZBOvlUFTNUMU3/hhJwvIo6VUB\nzjMHU2KJVwKBgDHqT2MAcGIYqGjZPhG2ZaBxZe1XIyZdy8Wp3IxZBzApoXPridjv\nXX12uCupjQFTAuHocyxLfYkIYUS4owDhHLbK3w4Lp/Tv5N9Fa/wXfoHmz4gJTWCJ\ngQf1Wi/QwwL9kgCfEJjf9MYzNt2F0PG81fkbhdtcJBfLsROv7MllHYuZAoGAfsmU\nCNjucCw12Zm5T3pL9+YKYudlhxjbZQcS3E+cAKZjhNfK+qq4Fctbk/C95iGif0o3\n2/r/zY1CXt0tFfR60Jhp5vrG2oLvaR7MK9uwEP5L9IbUoCAmzAnx9wr7xukWheUN\nT+QcReqYb3sKOfqtJcBfyL0HzS5eUVQ1MQVvypUCgYEAmMNB+xZp8Vrwj4ZkHuyQ\nV5NdVLRaGIRfmIyDm8YG9t4A1CR6Ezs4ZB+2kPSkoZFuZcU9iRKXn2F9jIknuW49\nzqt2VLtGBPj3UGwB+Tc4Q+k/Ytcz6bZXn8BIvBG15fNXpMFN6lekdvgZHV2wWvHJ\nbIJFZOVb0MhDqKNtJ2e2J+E=\n-----END PRIVATE KEY-----\n",
+  "client_email": "bot-darpepro@automatizacion-darpepro.iam.gserviceaccount.com",
+  "client_id": "104849416098488477184",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bot-darpepro%40automatizacion-darpepro.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+        
 
         # Limpiamos la clave por si acaso
         info["private_key"] = info["private_key"].replace("\\n", "\n")
@@ -60,3 +62,4 @@ if st.button('🚀 ¡PUBLICAR SIGUIENTE PRODUCTO AHORA!', use_container_width=Tr
         if producto:
             st.success(f"✅ ¡Hecho! '{producto}' enviado.")
             st.balloons()
+
